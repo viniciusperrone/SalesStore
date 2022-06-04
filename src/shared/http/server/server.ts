2d750c134a27a6665fc6a6958f7241bfc915389e
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-06-04 13:48:56 -0300

    2nd: database connection established

---
+++ src/shared/http/server/server.ts
@@ -0,0 +1,41 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import AppError from '@shared/errors/AppError';
+import { dataSource } from '@shared/typeorm';
+import router from '../routes';
+
+dotenv.config();
+
+dataSource.initialize().then(() => {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(router);
+
+  app.use(
+    (
+      error: Error,
+      request: Request,
+      response: Response,
+      next: NextFunction,
+    ) => {
+      if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+          status: 'error',
+          message: error.message,
+        });
+      }
+
+      return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+      });
+    },
+  );
+  app.listen(3333, () =>
+    console.log(`Server started on port ${process.env.SERVER_PORT || 3333}`),
+  );
+});
