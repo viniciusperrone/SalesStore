d9737eaf52bcb7b864be095c26cc76557d12f270
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-06-05 12:12:09 -0300

    4th: Products -controllers

---
+++ src/modules/products/services/index.ts
@@ -0,0 +1,13 @@
+import CreateProductService from './CreateProductService';
+import ListProductsService from './ListProductsService';
+import ShowProductService from './ShowProductService';
+import UpdateProductService from './UpdateProductService';
+import DeleteProductService from './DeleteProductService';
+
+export {
+  CreateProductService,
+  ListProductsService,
+  ShowProductService,
+  UpdateProductService,
+  DeleteProductService,
+};
