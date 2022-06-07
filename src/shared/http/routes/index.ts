fa6fe07cd73d23831dd517365da735c75e61b74f
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-06-07 11:26:56 -0300

    14th: Profile -User management completed

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import sessionRouter from '@modules/users/routes/session.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
+import profileRouter from '@modules/users/routes/profile.routes';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use('/products', productsRouter);
 router.use('/user', usersRouter);
 router.use('/sessions', sessionRouter);
 router.use('/password', passwordRouter);
+router.use('/profile', profileRouter);
 
 export default router;
