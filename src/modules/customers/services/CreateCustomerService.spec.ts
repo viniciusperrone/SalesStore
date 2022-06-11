822f53e356dd81aeeb80cb49150c4f84dabedeb8
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-06-11 14:22:20 -0300

    Docker added

---
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -0,0 +1,9 @@
+describe('CreateCustomer', () => {
+  it('Should be able to create a new customer', () => {
+    expect(1).toBe(1);
+  });
+
+  it('Should be able to create two new customers with the same email', () => {
+    expect(1).toBe(1);
+  });
+});
